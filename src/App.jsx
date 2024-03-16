5e8199a3ed423748f435bb2361d26087c71b2ae8
shubham <shubham.code.19@gmail.com>
2024-03-16 12:59:42 +0530

    covid tracker

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,13 @@
-import React, { useState } from 'react'
+import React from 'react'
 import "./App.css"
-import Header from './SGOOGLE/Header'
-import Footer from './SGOOGLE/Footer'
-import CreateNote from './SGOOGLE/CreateNote'
-import Note from './SGOOGLE/Note'
+import Covid from './Covid/Covid'
+
 const App = () => {
-  const[noteList,setNoteList]=useState([])
-
-  const add=(note)=>{
-    setNoteList((pVlaue)=>{
-        return [...pVlaue,note]
-    })
-  }
-
-  const removeOne=(id)=>{
-    setNoteList((pValue)=>{
-      let updateNoteLIst=pValue.filter((value,index)=>{
-          return index!==id
-      })
-      return updateNoteLIst
-    })
-  }
-
   return (
     <div>
-      <Header/>
-      <CreateNote add={add}/>
-
-        <div className="noteData">
-            
-      {noteList.map((value,index)=>{
-        return <Note 
-        key={index} 
-        id={index}
-        title={value.title}
-        content={value.content}
-        removeOne={removeOne}
-
-
-        />
-      })}
-        </div>
-      <Footer/>
+      <div className="mian">
+        <Covid/>
+      </div>
     </div>
   )
 }
